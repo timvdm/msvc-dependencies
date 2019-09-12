b06e1e4f7057edcc2e9d00e18ad241a9cfda4b3b
Noel O'Boyle <baoilleach@gmail.com>
2019-09-12 15:02:26 +0100

    Ensure that XTC/XPC compiles with the latest MSVC 2017

--- include/rpc/types.h
+++ include/rpc/types.h
@@ -56,10 +56,12 @@ typedef char     *caddr_t;
 
 #ifdef _MSC_VER
 /* MSVC does not define these types anywhere */
+#if (_MSC_VER < 1916) /* Now it does */
 typedef __int8            int8_t;
 typedef __int16           int16_t;
 typedef __int32           int32_t;
 typedef __int64           int64_t;
+#endif
 typedef unsigned __int8   u_int8_t;
 typedef unsigned __int16  u_int16_t;
 typedef unsigned __int32  u_int32_t;
